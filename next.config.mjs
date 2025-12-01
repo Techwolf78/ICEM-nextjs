4b34747428825e644ca2017616eee3229d458840
Sumedh Sangle <sanglesumedh15@gmail.com>
2025-12-01 15:57:57 +0530

    next config changed to use remote patterns instead of domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["img.youtube.com"],
+    // 1. Fix: Migrate "domains" to "remotePatterns"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+    ],
+    // 2. Fix: Explicitly allow quality="100" used in your banners
+    qualities: [25, 50, 75, 100],
   },
-
 };
 
 export default nextConfig;
